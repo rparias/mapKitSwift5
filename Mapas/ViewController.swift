dc207f78a6764abb8f11921fbe571ad7beca6055
Ronald Arias <ronald.arias@udla.edu.ec>
2019-11-12 14:08:31 -0500

    Agregados archivos

---
+++ Mapas/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Mapas
+//
+//  Created by Ronald Arias on 11/12/19.
+//  Copyright © 2019 Ronald Arias. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+    }
+
+
+}
+
